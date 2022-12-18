4ce43a339166ea2e8faee1351244dd89b87142ad
Daniil <daniilkharitonov1990@gmail.com>
2022-12-18 23:14:39 +0500

    Добавлена задача №2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 2: 
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число
+большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+*/
+
+/* Алгоритм решения:
+- Вывод в консоль команды на ввод двух чисел;
+- Задание переменных для данных чисел и ввод их в консоли;
+- Создание условия для сравнения данных чисел (число А > числа Б, число Б > числа А,
+ числа равны);
+- Вывод в консоль какое из чисел меньшее, какое большее.
+*/
+
+Console.WriteLine("Введите два числа");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.Write("Число " + numberA + " > " + "числа " + numberB);
+}
+else if (numberA < numberB)
+{
+    Console.Write("Число " + numberA + " < " + "числа " + numberB);
+}
+else if (numberA == numberB)
+{
+    Console.Write("Число " + numberA + " = " + "числу " + numberB);
+}
